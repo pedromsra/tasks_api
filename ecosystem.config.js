4bb334c59cc9935f1f8c8f9f74e55c8d86331c12
Pedro de Araújo <pedromsra@gmail.com>
2023-09-02 21:42:51 -0300

    db fix 7

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps : [{
     name: "app",
-    script: "index.js",
+    script: "./index.js",
     instances: "max",
     env: {
       NODE_ENV: "development",
--- knex.js
+++ knex.js
@@ -2,4 +2,6 @@ const config = require("./knexfile");
 const knex = require("knex");
 const connection = knex(config.development);
 
+console.log(connection)
+
 module.exports = connection;
